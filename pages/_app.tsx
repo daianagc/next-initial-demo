c2a81d5ad330ed88111bc11b1668062e532418f8
Daiana cucco <dgcucco91@gmail.com>
2024-05-06 12:39:26 -0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import "@/styles/globals.css";
+import { ReactElement, ReactNode } from "react";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => page);
+
+  // return (
+  //   <>
+  //     <Component {...pageProps} />
+  //   </>
+  // )
+  return getLayout(<Component {...pageProps} />);
 }
+
+export default MyApp;
